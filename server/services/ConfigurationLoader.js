6a7ad40cd930626c4833b46478898938adbed4c2
mymyoux <mymyoux@gmail.com>
2018-04-22 11:17:07 +0200

    config orders: myno/config < config/ < env.json

--- server/services/ConfigurationLoader.js
+++ server/services/ConfigurationLoader.js
@@ -14,22 +14,28 @@ export class ConfigurationLoader {
                 console.log(colors.red("env.json not found"));
                 reject();
             }
-            let config = JSON.parse(fs.readFileSync(localPath, 'utf8'));
+            let config = {}
+            //default config
             let corePath = path.join(__dirname,'../../config');
             fs.readdirSync(corePath).filter(file => {
                 return path.extname(file) == ".json";
             }).forEach((file) => {
                 this.mergeWithDefaultOptions(file.substring(0, file.length - path.extname(file).length), config, JSON.parse(fs.readFileSync(path.join(corePath, file), 'utf8')));
             });
+
             let globalConfigPath = storage_path("config.json");
             let globalConfig = fs.existsSync(globalConfigPath)?JSON.parse(fs.readFileSync(globalConfigPath, 'utf8')):{};
+            this.mergeWithDefaultOptions(null, config, globalConfig);
+            
             let globalPath = config_path();
             fs.readdirSync(globalPath).filter(file => {
                 return path.extname(file) == ".json";
             }).forEach((file) => {
                 this.mergeWithDefaultOptions(file.substring(0, file.length - path.extname(file).length), config, JSON.parse(fs.readFileSync(path.join(globalPath, file), 'utf8')));
             });
-            this.mergeWithDefaultOptions(null, config, globalConfig);
+            this.mergeWithDefaultOptions(null, config, JSON.parse(fs.readFileSync(localPath, 'utf8')));
+
+
             Configuration.readExternal(config);
             console.log(config);
             resolve();
@@ -47,7 +53,23 @@ export class ConfigurationLoader {
             }
             config = config[name];
         }
+        let keys = Object.keys(default_options);
+        for(let key of keys)
+        {
+            if(~key.indexOf('.'))
+            {
+                let parts = key.split('.');
+                let k = parts.shift();
+                if(!default_options[k])
+                {
+                    default_options[k] = {};
+                }
+                default_options[k][parts.join('.')] = default_options[key];
+                delete default_options[key];
+            }
+        }
         for (var p in default_options) {
+            
             if (config[p] == undefined) {
                 config[p] = default_options[p];
                 continue;
@@ -59,6 +81,8 @@ export class ConfigurationLoader {
             }
             if (typeof default_options[p] == "object") {
                 config[p] = this.mergeWithDefaultOptions(null, config[p], default_options[p]);
+            }else{
+                config[p] = default_options[p];
             }
         }
         return config;
