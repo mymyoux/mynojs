acd8e8b83bb16ad217c4ad6380e2b617b90420fb
mymyoux <mymyoux@gmail.com>
2018-05-11 15:14:31 +0200

    refactoring wrap/param

--- common/annotations/Param.js
+++ common/annotations/Param.js
@@ -1,4 +1,5 @@
-import { Functions } from "../../common/utils/Functions";
+import { Functions } from "../utils/Functions";
+import { wrap } from "./wrap";
 
 export function Param(name, options)
 {
@@ -17,13 +18,14 @@ export function Param(name, options)
 
     return function(target, key, descriptor)
     {
-        let paramList;
-        let index;
-        let indexParams;
-
         wrap(descriptor, function(parameters, next)
         {
-            let value = parameters[indexParams][options.name];
+            let params = parameters.get('params');
+            if(!params)
+            {
+                throw new Error('params must be in method parameters');
+            }
+            let value = params[options.name];
             
             if(options.default != undefined && value == null)
             {
@@ -69,57 +71,12 @@ export function Param(name, options)
                     }
                 }
             }
-            if(~index)
-            {
-                while(parameters.length<=index)
-                {
-                    parameters.push(undefined);
-                }
-                parameters.splice(index, 1, value);
-            }
+            params[options.name] = value;
+            parameters.replace(options.name, value);
             return next();
         });
-        paramList  = descriptor.value.parameters;
-        index = paramList.indexOf(options.name);
-        indexParams = paramList.indexOf("params");
-        if(!~indexParams)
-        {
-            throw new Error('params must be in method parameters');
-        }
         return descriptor;
     }
 }
 
   
-function wrap(descriptor, method)
-{
-
-    if(!descriptor.value.overrided)
-    {
-        let original = descriptor.value;
-        let wrapped = function()
-        {
-            let index = 0;
-            let current = wrapped.methods[index];
-            let parameters = Array.from(arguments);
-            let next = function()
-            {
-                index++;
-                if(wrapped.methods.length <= index)
-                {
-                    return original(...parameters);
-                }else
-                {
-                    return wrapped.methods[index](parameters, next);
-                }
-            }
-            return current(parameters, next);
-        };
-        wrapped.overrided = true;
-        wrapped.methods = [];
-        wrapped.original = original;
-        wrapped.parameters = Functions.getParameters(original);
-        descriptor.value = wrapped;
-    }
-    descriptor.value.methods.push(method);
-}
---
+++ common/annotations/wrap.js
@@ -0,0 +1,58 @@
+import { Functions } from "../utils/Functions";
+
+  
+export function wrap(descriptor, method)
+{
+
+    if(!descriptor.value.overrided)
+    {
+        let original = descriptor.value;
+        let wrapped = function()
+        {
+            let index = 0;
+            let current = wrapped.methods[index];
+            let parameters = Array.from(arguments);
+            parameters.replace = function(name, value)
+            {
+                let index = wrapped.parameters.indexOf(name);
+                if(~index)
+                {
+                    while(parameters.length<=index)
+                    {
+                        parameters.push(undefined);
+                    }
+                    parameters[index] = value;
+                }
+            }
+            
+            parameters.get = function(name)
+            {
+                let index = wrapped.parameters.indexOf(name);
+                if(~index)
+                {
+                    return parameters[index];
+                }
+                return null;
+            }
+
+            let next = function()
+            {
+                index++;
+                if(wrapped.methods.length <= index)
+                {
+                    return original(...parameters);
+                }else
+                {
+                    return wrapped.methods[index](parameters, next);
+                }
+            }
+            return current(parameters, next);
+        };
+        wrapped.overrided = true;
+        wrapped.methods = [];
+        wrapped.original = original;
+        wrapped.parameters = Functions.getParameters(original);
+        descriptor.value = wrapped;
+    }
+    descriptor.value.methods.push(method);
+}
